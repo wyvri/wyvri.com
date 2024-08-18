921406bf5279238b5bdbf6ef6bb14f31d752496c
Wyvern Kerr <wrennuin@gmail.com>
2024-08-18 16:26:12 -0500

    paste webpage onto webpage

---
+++ copy-from-webapp.sh
@@ -0,0 +1,4 @@
+curl https://wyvri.pythonanywhere.com/ > index.html
+curl https://wyvri.pythonanywhere.com/static/style.css > static/style.css
+curl https://wyvri.pythonanywhere.com/aboutme/ > aboutme/index.html
+curl https://wyvri.pythonanywhere.com/gallery/ > gallery/index.html
