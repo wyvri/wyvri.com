38f298f8b541b2e47084a4fda1440323291d7ccc
Wyvern Kerr <wrennuin@gmail.com>
2024-08-18 16:34:03 -0500

    add images from webapp to gallery page on website

--- copy-from-webapp.sh
+++ copy-from-webapp.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+source_url="https://wyvri.pythonanywhere.com"
+
 curl https://wyvri.pythonanywhere.com/ > index.html
 curl https://wyvri.pythonanywhere.com/static/style.css > static/style.css
 curl https://wyvri.pythonanywhere.com/aboutme/ > aboutme/index.html
 curl https://wyvri.pythonanywhere.com/gallery/ > gallery/index.html
+
+for img in $( grep src gallery/index.html | sed 's/.*src="//' | sed 's/".*//' ); do
+   echo $img "..."
+   curl $source_url$img > ".$img"
+done
